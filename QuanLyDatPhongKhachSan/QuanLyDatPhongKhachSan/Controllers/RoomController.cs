d75b0a489f2603835201f1dab34a618caa9ebdba
thanhtruc0606 <thanhtruc060623@gmail.com>
2024-02-23 22:36:03 +0700

    .

---
+++ QuanLyDatPhongKhachSan/QuanLyDatPhongKhachSan/Controllers/RoomController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace QuanLyDatPhongKhachSan.Controllers
+{
+    public class RoomController : Controller
+    {
+        // GET: Room
+        public ActionResult RoomDetail()
+        {
+            return View();
+        }
+    }
+}
---
+++ QuanLyDatPhongKhachSan/QuanLyDatPhongKhachSan/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="QuanLyDatPhongKhachSan.MvcApplication" Language="C#" %>
